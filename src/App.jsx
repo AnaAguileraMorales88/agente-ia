e9b2920a20b8efafbf773527f364961b2e41fa97
Ana Aguilera <anitaaguilera@hotmail.com>
2026-03-23 14:04:46 +0100

    feat: instalado react + vite y tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind funciona 🎉
+      </h1>
+    </main>
+  )
+}
+
+export default App
